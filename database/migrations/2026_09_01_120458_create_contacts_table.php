19c85ba9e840fbdaa0ed4a09f4d23148f522ca9b
damienblaumuller@gmail.com <damienblaumuller1@gmail.com>
2026-09-01 14:29:47 +0200

    Contacts migration with name, email, phone, position

--- database/migrations/2026_09_01_120458_create_contacts_table.php
+++ database/migrations/2026_09_01_120458_create_contacts_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('company_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('position')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('contacts');
