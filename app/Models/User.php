0a778036e84f362e53ba4ae2f9ac4d2cf0138dd5
damienblaumuller@gmail.com <damienblaumuller1@gmail.com>
2026-09-02 17:14:35 +0200

    Add function application to user models

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Str;
 use Laravel\Fortify\Contracts\PasskeyUser;
 use Laravel\Fortify\PasskeyAuthenticatable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property int $id
@@ -56,7 +57,11 @@ class User extends Authenticatable implements PasskeyUser
         $initials = Str::initials($this->name, true);
 
         return Str::length($initials) > 1
-            ? Str::substr($initials, 0, 1).Str::substr($initials, -1)
+            ? Str::substr($initials, 0, 1) . Str::substr($initials, -1)
             : $initials;
+    }
+    public function applications(): HasMany
+    {
+        return $this->hasMany(Application::class);
     }
 }
