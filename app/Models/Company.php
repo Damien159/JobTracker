9a25653e66b350fe2f681cc5f1ca824a5aca6bd0
damienblaumuller@gmail.com <damienblaumuller1@gmail.com>
2026-09-02 16:44:19 +0200

    Add Models with and write Models for Company

---
+++ app/Models/Company.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Company extends Model
+{
+    protected $fillable = ['name', 'website', 'notes'];
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class);
+    }
+
+    public function applications(): HasMany
+    {
+        return $this->hasMany(Application::class);
+    }
+}
