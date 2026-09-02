bee5f93a3ab3a047372cdc9b37284cef815fa8f7
damienblaumuller@gmail.com <damienblaumuller1@gmail.com>
2026-09-02 17:10:53 +0200

    Document model

--- app/Models/ApplicationDocument.php
+++ app/Models/ApplicationDocument.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ApplicationDocument extends Model
 {
-    //
+    protected $fillable = [
+        'application_id', 'type', 'original_filename',
+        'path', 'mime_type', 'size', 'uploaded_at',
+    ];
+
+    protected $casts = [
+        'uploaded_at' => 'datetime',
+    ];
+
+    public function application(): BelongsTo
+    {
+        return $this->belongsTo(Application::class);
+    }
 }
+
