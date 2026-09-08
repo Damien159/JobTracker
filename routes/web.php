e6a6df4b96142648a14e150ea867542c70e341e6
damienblaumuller@gmail.com <damienblaumuller1@gmail.com>
2026-09-08 20:47:19 +0200

    Route dashboard to new Livewire component, remove old placeholder view

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/login')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::get('dashboard', \App\Livewire\Dashboard::class)->name('dashboard');
     Route::get('/applications', \App\Livewire\Applications\Index::class)->name('applications.index');
     Route::get('/applications/{applicationId}', \App\Livewire\Applications\Show::class)->name('applications.show');
 });
